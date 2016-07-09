e8aaa1d3ff9ea0484c36ca2f8adc787ce9d24f45
lastromanticx <giladbarkan@live.com>
2016-07-08 23:11:47 -0400

    added user show view, item add view

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,32 @@
+class ItemsController < ApplicationController
+  get '/items/new' do
+    redirect_if_not_logged_in
+    erb :'/items/new'
+  end
+
+  post '/items' do
+    if params[:item][:name].match(/^\s*$/)
+      redirect '/items/new?error=Please name the item.'
+    end
+
+    item = Item.create(params[:item])
+
+    current_user.items << item 
+    if !params[:property][:name].match(/^\s*$/)
+      item.properties << Property.create(params[:property])
+    end
+
+    redirect "/items/#{item.id}"
+  end
+
+  get '/items/:id' do
+    redirect_if_not_logged_in
+    
+    @item = Item.find(params[:id])
+    if @item.share == "private"
+      redirect '/users'
+    else
+      erb :'/items/show'
+    end
+  end
+end
